4cc0d67db4cee062e8e7367255f9a58c6fceec74
Lucas Spila Sereno <luca.up@yahoo.com>
2026-09-01 21:57:41 -0300

    Add CRUD of Pharmacy

---
+++ mvp/Entities/Pharmacy.cs
@@ -0,0 +1,39 @@
+using mvp.ValueObjects;
+
+namespace mvp.Entities
+{
+    public class Pharmacy : BaseEntity
+    {
+        public Name name {get; private set; }
+        public Address address { get; private set; }
+        public CNPJ cnpj {get; private set; }
+        public PhoneNumber phoneNumber {get; private set;}
+        //Futuramente adicionar a chave estrangeira de Estoque
+
+        protected Pharmacy()
+        {
+            name = null!;
+            address = null!;
+            cnpj = null;
+            phoneNumber = null;
+        }
+
+        public User(Name name, Address address, CNPJ cnpj, PhoneNumber phoneNumber)
+        {
+            this.name = name;
+            this.address = address;
+            this.cnpj = cnpj;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void Update(Name name, Address address, CNPJ cnpj, PhoneNumber phoneNumber)
+        {
+            this.name = name;
+            this.address = address;
+            this.cnpj = cnpj;
+            this.phoneNumber = phoneNumber;
+
+            UpdateTimestamps();
+        }
+    }
+}
